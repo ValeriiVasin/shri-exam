25b68d606753bbf85d260c010eb108c280d1aa82
Valeriy Vasin <vv@anahoret.com>
2012-10-14 15:35:41 +0400

    Add templates module.

---
+++ htdocs/js/modules/templates.js
@@ -0,0 +1,31 @@
+/**
+ * @fileOverview Module is responsible for unobtrusive templates loading
+ *
+ * @todo  Realize ajax loading of template, based on it's name
+ */
+;(function ($) {
+
+    App.modules.define('templates', function (app) {
+        var templates = {}; // cache for templates
+
+        function get(name) {
+            var _element = null;
+
+            if (!templates[name]) {
+                _element = $('script[name="'+ name +'"]');
+                if (_element.length === 0) {
+                    throw new Error('Template ' + name + ' not found');
+                }
+                templates[name] = _.template( _element.html() );
+            }
+
+            return templates[name];
+        }
+
+        return {
+            init: $.noop,
+            get: get
+        };
+    });
+
+}(jQuery));
